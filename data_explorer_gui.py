f29dc7e37ccb5363b470b53fb3a30c303f68ecd4
Jenerth Rodriguez <Connerjamestill@gmail.com>
2023-06-14 01:56:34 -0400

    using matplot to create graph

--- data_explorer_gui.py
+++ data_explorer_gui.py
@@ -1,41 +1,74 @@
-import csv
-from tkinter import *
 import tkinter as tk
 from tkinter import filedialog, messagebox
 import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
-def read_csv_file():
-    # Open file dialog to select a CSV file
-    file_path = filedialog.askopenfilename(filetypes=[("CSV Files", "*.csv")])
+class WearableSensorDataExplorer:
+    def __init__(self, root):
+        self.root = root
+        self.df = None
+        self.time_box = None
 
-    # Read the selected CSV file
-    df = pd.read_csv(file_path)
+        self.create_widgets()
 
-    # Display the content in a messagebox
-    tk.messagebox.showinfo("CSV Content", df.to_string())
+    def create_widgets(self):
+        load_button = tk.Button(self.root, text="Load Sensor Data", command=self.load_sensor_data)
+        load_button.pack()
 
-main = Tk()
-main.title("CS50")
-main.geometry("500x550")
+        self.fig, self.ax = plt.subplots()
+        self.canvas = FigureCanvasTkAgg(self.fig, master=self.root)
+        self.canvas.get_tk_widget().pack()
 
-# label1 = Label(text="Enter Username:")
-# label1.place(x=30, y=30)
-# label1.config(bg='lightgreen', padx=0)
+        self.time_box = tk.Entry(self.root)
+        self.time_box.pack()
 
-# username = Entry(text="")
-# username.place(x=150, y=40, width=200, height=25)
+    def load_sensor_data(self):
+        # Open file dialog to select a CSV file
+        file_path = filedialog.askopenfilename(filetypes=[("CSV Files", "*.csv")])
 
-# label2 = Label(text="Enter Password:")
-# label2.place(x=30, y=80)
-# label2.config(bg='lightgreen', padx=0)
+        if file_path:
+            try:
+                # Load the CSV file into a DataFrame
+                self.df = pd.read_csv(file_path)
 
-# password = Entry(text="")
-# password.place(x=150, y=80, width=200, height=25)
+                # Perform data analysis and visualization
+                self.analyze_and_visualize_data()
 
-# button = Button(text="ADD", command=add_new_user)
-# button.place(x=150, y=120, width=75, height=35)
+                messagebox.showinfo("Success", "Sensor data loaded successfully!")
 
-read_csv_button = Button(main, text="Read CSV File", command=read_csv_file)
-read_csv_button.pack(pady=10)
+            except Exception as e:
+                messagebox.showerror("Error", f"Failed to load sensor data: {str(e)}")
 
-main.mainloop()
+    def analyze_and_visualize_data(self):
+        self.ax.clear()
+        time_input = self.time_box.get()
+        filtered_data = self.df
+
+        if time_input:
+            filtered_data = self.df.loc[self.df["Name"] == time_input]
+
+        statistics = filtered_data.describe()
+
+        messagebox.showinfo("Data Statistics", statistics.to_string())
+
+        x = filtered_data["Name"]
+        y = filtered_data["Age"]
+        z = filtered_data["Salary"]
+        self.ax.plot(x, y, z)
+        self.ax.set_xlabel("Name")
+        self.ax.set_ylabel("Age")
+        self.ax.set_ylabel("Salary")
+
+        # Format the plot
+        self.ax.set_title("Wearable Sensor Data")
+        self.fig.autofmt_xdate()
+
+        self.canvas.draw()
+
+window = tk.Tk()
+window.title("Wearable Sensor Data Explorer")
+
+data_explorer = WearableSensorDataExplorer(window)
+
+window.mainloop()
