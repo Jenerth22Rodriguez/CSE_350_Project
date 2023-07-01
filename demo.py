fba264792db5b1c1562af2cebb6dfe3c4a3e407d
Jenerth Rodriguez <Connerjamestill@gmail.com>
2023-07-01 14:37:48 -0400

    I'm removing unneccessay comments

--- demo.py
+++ demo.py
@@ -12,8 +12,8 @@ def upload_file():
     file_path = filedialog.askopenfilename(filetypes=file_types)
     
     if file_path:
-        df = pd.read_csv(file_path)  # Read the CSV file and create a DataFrame
-        display_dataframe(df)  # Display the DataFrame in the Treeview
+        df = pd.read_csv(file_path)  
+        display_dataframe(df)  
 
 def display_dataframe(df):
     clear_treeview()
@@ -66,52 +66,52 @@ def create_chart():
                 plt.title("Device Information")
                 plt.grid(True)
     
-                # Create a FigureCanvasTkAgg object and embed the figure in the Tkinter window
+              
                 canvas = FigureCanvasTkAgg(fig, master=window)
                 canvas.draw()
                 canvas.get_tk_widget().pack()
 
-# Create the main Tkinter window
+
 window = tk.Tk()
 window.title("CSV File Reader")
 window.geometry("750x400")
 
-# Load and set the background image
+
 background_image = Image.open("coding.jpeg")
 background_photo = ImageTk.PhotoImage(background_image)
 background_label = tk.Label(window, image=background_photo)
 background_label.place(x=0, y=0, relwidth=1, relheight=1)
 
-# Create and place the "Browse File" button
+
 btn_browse = tk.Button(window, text="Browse File", command=upload_file, height=2, width=20, bg='lightblue')
 btn_browse.place(x=175, y=300)
 
-# Create and place the "Delete" button
+
 btn_delete = tk.Button(window, text="Delete", command=delete_row, height=2, width=20, bg='lightcoral')
 btn_delete.place(x=375, y=300)
 
-# Create and place the "Clear The Screen" button
+
 btn_clear = tk.Button(window, text="Clear The Screen", command=clear_treeview, height=2, width=20, bg='silver')
 btn_clear.place(x=575, y=300)
 
-# Create and place the "Create Chart" button
+
 btn_chart = tk.Button(window, text="Create Chart", command=create_chart, height=2, width=20, bg='lightgreen')
 btn_chart.place(x=375, y=350)
 
-# Create and place the label for displaying the DataFrame dimensions
+
 lb2 = tk.Label(window, text="")
 lb2.pack()
 
-# Create the Treeview widget
+
 style = ttk.Style()
-style.configure("Treeview", borderwidth=100)  # Remove the default border
+style.configure("Treeview", borderwidth=100)  
 trv = ttk.Treeview(window, style="Treeview", show="headings")
 trv.pack(padx=10, pady=(0, 10))
 
-# Create and place the chart type dropdown menu
+
 combo_chart = ttk.Combobox(window, values=["Line"])
 combo_chart.current(0)  # Set default value to "Line"
 combo_chart.pack(padx=10, pady=(0, 10))
 
-# Run the Tkinter event loop
+
 window.mainloop()
