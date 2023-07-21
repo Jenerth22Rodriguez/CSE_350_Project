c785f6fb3af242b4ed9ab1639ada741f5ff78f77
Jenerth Rodriguez <Connerjamestill@gmail.com>
2023-07-21 01:17:43 -0400

    Final demo

--- Chart.py
+++ Chart.py
@@ -1,61 +1,56 @@
 from numpy import mean, median
 import pandas as pd
 import tkinter as tk
-import tkinter.ttk as ttk
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg as Canvas
 from matplotlib.figure import Figure
-import statistics
 
-
-class chart():
-    def __init__(self, chartType: str, dataX, dataY, y):
+class Chart:
+    def __init__(self, chartType: str, dataX, dataY):
         self.chartType = chartType
         self.dataX = dataX
         self.dataY = dataY
-        self.y = y
-        
+
     def delete_frame(self):
         self.chart_and_button.destroy()
         self.static.destroy()
-    
+        self.delete_static()
+
     def delete_static(self):
         for widget in self.static.winfo_children():
             widget.destroy()
-    
+
     def create_stats_anal(self):
-        self.label = tk.Label(self.static, text=" Statistics of " + str(self.y))
-        self.label.config(font=('', 8, 'bold'))
-        self.label.pack()
-        self.label_max = tk.Label(self.static, text=" ")
-        self.label_min = tk.Label(self.static, text=" ")
-        self.label_median = tk.Label(self.static, text=" ")
-        self.label_mean = tk.Label(self.static, text=" ")
+        if len(list(self.static.winfo_children())) == 0:
+            self.label = tk.Label(self.static, text=" Statistics of " + str(self.dataY.name))
+            self.label.config(font=('', 8, 'bold'), background="light grey")
+            self.label_max = tk.Label(self.static, text=" ", background="light grey")
+            self.label_min = tk.Label(self.static, text=" ", background="light grey")
+            self.label_median = tk.Label(self.static, text=" ", background="light grey")
+            self.label_mean = tk.Label(self.static, text=" ", background="light grey")
 
-        self.label_max["text"] = 'Max: ' + str(max(self.dataY))
-        self.label_min["text"] = 'Min: ' + str(min(self.dataY))
-        self.label_median["text"] = 'Median: ' + str(median(self.dataY))
-        self.label_mean["text"] = 'Mean: ' + str(mean(self.dataY))
-        btn_del = tk.Button(self.static, text="Delete Statistics", command=self.delete_static, height=1, width=15, bg='lightblue', state="normal")
+            self.label_max["text"] = 'Max: ' + str(max(self.dataY))
+            self.label_min["text"] = 'Min: ' + str(min(self.dataY))
+            self.label_median["text"] = 'Median: ' + str(median(self.dataY))
+            self.label_mean["text"] = 'Mean: ' + str(mean(self.dataY))
 
-        self.label_max.pack()
-        self.label_min.pack()
-        self.label_median.pack()
-        self.label_mean.pack()
-        btn_del.pack()
-        
+            btn_del = tk.Button(self.static, text="Delete Statistics", command=self.delete_static, height=1, width=15, bg='lightblue', state="normal")
+
+            self.label.pack()
+            self.label_max.pack()
+            self.label_min.pack()
+            self.label_median.pack()
+            self.label_mean.pack()
+            btn_del.pack()
+
     def display(self, parent, sidebar):
-        self.static = tk.Frame(sidebar)
-        self.static.pack(fill="y")
-
+        self.static = tk.Frame(sidebar, background="light grey")
+        self.static.pack()
         self.chart_and_button = tk.Frame(parent)
         self.chart_and_button.pack(fill="y")
-        self.fig = Figure(figsize=(22, 3), dpi=55, tight_layout=True)
+        self.fig = Figure(figsize=(12, 2), dpi=100, tight_layout=True)
         self.plot = self.fig.add_subplot(111)
         self.canvas = Canvas(self.fig, self.chart_and_button)
         self.canvas.get_tk_widget().pack(side="left")
-
-        label_chart = tk.Label(self.chart_and_button, text=self.y)
-        label_chart.pack()
 
         btn_stat = tk.Button(self.chart_and_button, text="Show Statistics", command=self.create_stats_anal, height=2, width=20, bg='lightblue')
         btn_stat.pack()
@@ -69,5 +64,7 @@ class chart():
             self.plot.bar(self.dataX, self.dataY)
         elif self.chartType == "Scatter":
             self.plot.scatter(self.dataX, self.dataY)
-        
+
+        self.plot.set_xlabel("Datetime")
+        self.plot.set_ylabel(str(self.dataY.name)) 
         self.canvas.draw()
--- demo.py
+++ demo.py
@@ -6,13 +6,13 @@ from tkinter import filedialog
 from PIL import ImageTk, Image
 from tkinter import messagebox
 import pandas as pd
+import datetime
 import Chart
 from tkinter import *
 import datetime
-
+import statistics
 
 df = None  # Establishing global dataframe variable
-
 
 def upload_file():
     global df
@@ -20,139 +20,135 @@ def upload_file():
     file_path = filedialog.askopenfilename(filetypes=file_types)
 
     if file_path:
-        df = pd.read_csv(
-            file_path,
-            parse_dates=['Datetime (UTC)'],
-            date_parser=lambda x: pd.to_datetime(x, format='%Y-%m-%dT%H:%M:%SZ')
-        )
+        df = pd.read_csv(file_path)
+        df["Datetime (UTC)"] = pd.to_datetime(df["Datetime (UTC)"], format="%Y-%m-%dT%H:%M:%SZ")
+        df['Datetime'] = df['Datetime (UTC)'].dt.tz_localize('UTC')
+        df['time'] = df['Datetime']
         combo_datastream["values"] = list(df.columns)[3:]  # Populating datastream combobox
         combo_datastream.current(0)  # Setting default datastream combobox value
+        combo_time["values"] = ["UTC", "US Eastern", "US Central", "US Mountain", "US Pacific", "US Alaskan", "US Hawaiian"]
+        combo_time.current(0)
+        update_start_and_end_time_comboboxes()
         btn_chart["state"] = "normal"  # Enabling create chart button
         btn_clear["state"] = "normal"  # Enabling clear participant button
 
+def timezone_update(event):
+    global df
+
+    if combo_time.get() == "UTC":
+        df['time'] = df['Datetime']
+
+    elif combo_time.get() == "US Eastern":
+        df['time'] = df['Datetime'].dt.tz_convert('US/Eastern')
+
+    elif combo_time.get() == "US Mountain":
+        df['time'] = df['Datetime'].dt.tz_convert('US/Mountain')
+
+    elif combo_time.get() == "US Central":
+        df['time'] = df['Datetime'].dt.tz_convert('US/Central')
+
+    elif combo_time.get() == "US Alaska":
+        df['time'] = df['Datetime'].dt.tz_convert('US/Alaska')
+
+    elif combo_time.get() == "US Pacific":
+        df['time'] = df['Datetime'].dt.tz_convert('US/Pacific')
+
+    elif combo_time.get() == "US Hawaiian":
+        df['time'] = df['Datetime'].dt.tz_convert('US/Hawaii')
+    
+    update_start_and_end_time_comboboxes()
+
+def update_start_and_end_time_comboboxes():
+    combo_start_time["values"] = list(df["time"])
+    combo_start_time.current(0)
+    combo_end_time["values"] = list(df["time"])
+    combo_end_time.current(len(list(df["time"])) - 1)
 
 def destroy_charts():
     for widget in chart_space.winfo_children():
         widget.destroy()
 
-
 def clear_participant():
     destroy_charts()
-    btn_chart["state"] = "disable"
-    btn_clear["state"] = "disable"
+    btn_chart["state"] = "disabled"
+    btn_clear["state"] = "disabled"
     combo_datastream["values"] = ""
     combo_datastream.set('')
-
+    combo_start_time["values"] = ""
+    combo_start_time.set('')
+    combo_end_time["values"] = ""
+    combo_end_time.set('')
+    combo_time["values"] = ""
+    combo_time.set('')
 
 def create_chart():
     global df
+    timeframe = df["time"][combo_start_time.current():combo_end_time.current()]
+    data = df[combo_datastream.get()][combo_start_time.current():combo_end_time.current()]
+    chart = Chart.chart(combo_chart.get(), timeframe, data)
+    chart.display(chart_space, sidebar)
 
-    df['Datetime'] = df['Datetime (UTC)'].dt.tz_localize('UTC')
+def update_combo_end_time(event):
+    combo_end_time["values"] = list(df["time"])[list(df["time"]).index(pd.to_datetime(combo_start_time.get(), format="ISO8601")):]
 
-    if combo_time.get() == "UTC":
-        chart = Chart.chart(
-            combo_chart.get(),
-            df['Datetime (UTC)'],
-            df[combo_datastream.get()],
-            combo_datastream.get()
-        )
-        chart.display(chart_space, sidebar)
-
-    elif combo_time.get() == "US Eastern":
-        df['time'] = df['Datetime'].dt.tz_convert('US/Eastern')
-        chart = Chart.chart(
-            combo_chart.get(),
-            df['time'],
-            df[combo_datastream.get()],
-            combo_datastream.get()
-        )
-        chart.display(chart_space, sidebar)
-
-    elif combo_time.get() == "US Mountain":
-        df['time'] = df['Datetime'].dt.tz_convert('US/Mountain')
-        chart = Chart.chart(
-            combo_chart.get(),
-            df['time'],
-            df[combo_datastream.get()],
-            combo_datastream.get()
-        )
-        chart.display(chart_space, sidebar)
-
-    elif combo_time.get() == "US Central":
-        df['time'] = df['Datetime'].dt.tz_convert('US/Central')
-        chart = Chart.chart(
-            combo_chart.get(),
-            df['time'],
-            df[combo_datastream.get()],
-            combo_datastream.get()
-        )
-        chart.display(chart_space, sidebar)
-
-    elif combo_time.get() == "US Alanskan":
-        df['time'] = df['Datetime'].dt.tz_convert('US/Alaska')
-        chart = Chart.chart(
-            combo_chart.get(),
-            df['time'],
-            df[combo_datastream.get()],
-            combo_datastream.get()
-        )
-        chart.display(chart_space, sidebar)
-
-    elif combo_time.get() == "US Pacific":
-        df['time'] = df['Datetime'].dt.tz_convert('US/Pacific')
-        chart = Chart.chart(
-            combo_chart.get(),
-            df['time'],
-            df[combo_datastream.get()],
-            combo_datastream.get()
-        )
-        chart.display(chart_space, sidebar)
-
-    elif combo_time.get() == "US Hawaiian":
-        df['time'] = df['Datetime'].dt.tz_convert('US/Hawaii')
-        chart = Chart.chart(
-            combo_chart.get(),
-            df['time'],
-            df[combo_datastream.get()],
-            combo_datastream.get()
-        )
-        chart.display(chart_space, sidebar)
-
+def update_combo_start_time(event):
+    combo_start_time["values"] = list(df["time"])[:list(df["time"]).index(pd.to_datetime(combo_end_time.get(), format="ISO8601")) + 1]
 
 window = tk.Tk()
 window.title("CSV File Reader")
 window.geometry("1920x1080")
 
-sidebar = tk.Frame(window)
+sidebar = tk.Frame(window, background="light grey")
 sidebar.pack(side="left", fill="y")
 
 chart_space = tk.Frame(window)
 chart_space.pack(side="right", fill="y")
 
 btn_browse = tk.Button(sidebar, text="Browse File", command=upload_file, height=2, width=20, bg='lightblue')
-btn_browse.pack()
+btn_browse.pack(padx=10, pady=10)
 
+label_start = Label(sidebar, text="Select Start Time", background="light grey")
+label_start.pack()
+combo_start_time = ttk.Combobox(sidebar)
+combo_start_time.pack(padx=10, pady=(0, 10))
+combo_start_time.bind("<<ComboboxSelected>>", update_combo_end_time)
+
+label_end = Label(sidebar, text="Select End Time", background="light grey")
+label_end.pack()
+combo_end_time = ttk.Combobox(sidebar)
+combo_end_time.pack(padx=10, pady=(0, 10))
+combo_end_time.bind("<<ComboboxSelected>>", update_combo_start_time)
+
+label_datastream = Label(sidebar, text="Select Datastream", background="light grey")
+label_datastream.pack()
 combo_datastream = ttk.Combobox(sidebar, values="", state="readonly")
-combo_datastream.pack()
+combo_datastream.pack(padx=10)
 
 btn_delete = tk.Button(sidebar, text="Delete All Charts", command=destroy_charts, height=2, width=20, bg='lightcoral')
-btn_delete.pack()
+btn_delete.pack(padx=10, pady=10)
 
 btn_clear = tk.Button(sidebar, text="Clear Participant", command=clear_participant, height=2, width=20, bg='silver', state="disabled")
-btn_clear.pack()
+btn_clear.pack(padx=10, pady=10)
 
 btn_chart = tk.Button(sidebar, text="Create Chart", command=create_chart, height=2, width=20, bg='lightgreen', state="disabled")
-btn_chart.pack()
+btn_chart.pack(padx=10, pady=10)
 
+label_chart = Label(sidebar, text="Select Chart Type", background="light grey")
+label_chart.pack()
 combo_chart = ttk.Combobox(sidebar, values=["Line", "Bar", "Scatter"])
 combo_chart.current(0)  # Set default value to "Line"
 combo_chart.pack(padx=10, pady=(0, 10))
 
-label_time = Label(sidebar, text="Select Time")
+label_time = Label(sidebar, text="Select Timezone", background="light grey")
 label_time.pack()
+combo_time = ttk.Combobox(sidebar)
+combo_time.pack(padx=10, pady=(0, 10))
+combo_time.bind("<<ComboboxSelected>>", timezone_update)
 
-combo_time = ttk.Combobox(sidebar, values=["UTC", "US Eastern", "US Central", "US Mountain", "US Pacific", "US Alanskan", "US Hawaiian"])
-combo_time.current(0)
-combo_time.pack()
+stats = tk.Frame(sidebar, background="light grey")
+stats.pack(fill="y")
+
+style = ttk.Style()
+style.configure("Treeview", borderwidth=100)
 
 window.mainloop()
